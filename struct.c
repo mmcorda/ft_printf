7c9b8ee1ef22434b73cd0ac28e536a5870400328
mcorda <77837096+mmcorda@users.noreply.github.com>
2021-03-07 16:31:18 +0100

    Update struct.c

--- struct.c
+++ struct.c
@@ -11,3 +11,10 @@ t_format        ft_init_struct(void)
         format.type = 0;
         return (format);
 }
+
+void    fill_struct_element(char *str, t_format *format)
+{
+        fill_type(str, format);
+        fill_dash_indicator(str, format);
+        fill_zero_indicator(str, format);
+}
