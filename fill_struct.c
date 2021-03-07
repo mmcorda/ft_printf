bf7eed433dae1bfbc41f5f13830372cea3cc2f78
mcorda <77837096+mmcorda@users.noreply.github.com>
2021-03-07 16:15:38 +0100

    Create fill_struct.c

---
+++ fill_struct.c
@@ -0,0 +1,44 @@
+#include "../ft_printf.h"
+
+int   fill_type(char *str, t_format)
+{
+      int type;
+  
+      type = get_conv_type(str);
+      format->type = type;
+      return (type);
+}
+
+int   fill_dash_indicator(str, t_format *format)
+{
+      int dash;
+  
+      dash = find_char(str, '-');
+      if (dash == 1)
+      {
+              format->indicator[1] = 1;
+      }
+      return (dash);
+}
+
+int fill_zero_indicator(char *str, t_format *format)
+{
+        if (found_char(str, '-') == 1)
+        {
+                format.indicator[0] = 0;
+                format.indicator[1] = 1;
+                return (0);
+        }
+        else
+        {
+                //Voir cette partie pour le cas d'erreur
+                if (str[1] == '0')
+                {
+                        format.indicator[0] = 1;
+                        format.indicator[1]
+                        return (1);
+                }
+        }
+        return (0);
+}
+        
