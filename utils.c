7ff5ce113094ab00225145d94ed292533eaae532
mcorda <77837096+mmcorda@users.noreply.github.com>
2021-03-07 15:32:48 +0100

    Create utils.c

---
+++ utils.c
@@ -0,0 +1,15 @@
+#include "../ft_printf.h"
+
+int   find_char(char *str, char c)
+{
+      int i;
+  
+      i = 0;
+      while (str[i])
+      {
+              if (str[i] == c)
+                      return (1);
+              i++;
+      }
+      return (0);
+}
