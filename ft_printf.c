d4c1fb8427ea522fa7620731b38ff840e3baa446
mcorda <77837096+mmcorda@users.noreply.github.com>
2021-03-13 15:57:31 +0100

    Update ft_printf.c

--- ft_printf.c
+++ ft_printf.c
@@ -1,27 +1,64 @@
 #icnclude "ft_printf.h"
 
-void    ft_putchar(char c)
+//Verifier qu on a bien le bon nombre d element dans va_list?
+//Ajouter printed_chars dans la structure ?
+//remettre en ptr le spec , retester
+int     parse(const char *format, va_list arg_ptr)
 {
-        write(1, &c, 1);
+        t_format        *spec;
+        int             len;
+        int             printed_chars;
+        
+        len = 0;
+        spec = ft_init_struct();
+        //marche pas pour %
+        //printf("\nLa string est : "%s\n", &format[i]
+        fill_struct(format, spec);//attention a ne pas envoyer des mauvais type
+        if (found_char(format))
+                handle_star(spec, arg_ptr);
+        printed(spec, arg_ptr);
+        printed_char = spec->printed_chars;
+        return(printed_chars);
 }
-  
+
+//Voir si on devrait ajouter une variable pour suivre sur quel caractere on se trouve
+//Attention differencier i et de printed chars
+//Attention tests plus ok nocrash null spec / ajouter fonction qui verifie
+//finir les tests pc 0 15
 int     ft_printf(const char *format, ...)
 {
-        int   i;
-        int   c;
-//On creer un compteur qui va se souvenir de combien de % on a croiser.
+        int             printed_chars;
+        va_list         arg_ptr;
+        int                     i;
+        //t_format      spec;
+        
+        printed_chars = 0;
         i = 0;
-        count = 0;
-        //va_start(args, ?)
+        //spec = ft_init_struct()
+        //printstruct(spec);
+        va_start(arg_ptr, format);
+//On creer un compteur qui va se souvenir de combien de % on a croiser.
         while (format[i])
         {
-                  if (format[i] != '%')
+                  if (format[i] == '%')
                   {
-                          ft_putchar(format[i]);
+                          if (!(is_correct_spec(&format[i])))
+                                  return (0);
+                          printed_chars += parse(&format[i], arg_ptr); //Attention valeur de retour pas bonne/ ok
+                          i += len_util_end_format(&format[i]);
                   }
                   else
                   {
-                          count++;
+                          ft_putchar(format[i]);
+                          printed_chars++;
+                  }
+                  i++;
+        }
+        va_end(arg_ptr);
+        return (printed_chars);
+}
+
+/*                          count++;
                           va_args(args, int);
                           //incrementer va_args?
                           i++;
@@ -46,3 +83,4 @@ int     ft_printf(const char *format, ...)
         va_end(args);
         return (1);
 }
+*/
